0d083664c0a12afa75195ddf94872dbe998ff51c
levonbr2006 <49477948+ybrutyan@users.noreply.github.com>
2021-09-25 22:34:18 +0400

    don't working grassCount and grassEaterCount

--- script.js
+++ script.js
@@ -1,143 +1,53 @@
-let grassArr = []; // {}, {},{}
-let grassEaterArr = []
-let gazanikArr = []
-let atomikBomb = []
-let findArr = []
-let atArr = []
-
-
-
-let matrix = []
-let n = 30
-let arrX = []
-let arrY = []
-let side = 20
-
-for (let i = 0; i < n; i++) {
-  matrix[i] = []
-  for (let j = 0; j < n; j++) {
-    matrix[i][j] = 0
-  }
-}
-  
-for (let i = 0; i < n*n / 2; i++) {
-  let randomX = Math.floor(Math.random() * n)
-  let randomY = Math.floor(Math.random() * n)
-  matrix[randomX][randomY] = 1
-  arrX.push(randomX)
-  arrY.push(randomY)
-}
-for (let i = 0; i < n*n / 50; i++) {
-  let randomX = Math.floor(Math.random() * n)
-  let randomY = Math.floor(Math.random() * n)
-  for(let k = 0; k < arrX.length; k++){
-    if(arrX[k] != randomX && arrY[k] != randomY){
-      matrix[randomX][randomY] = 2
-      arrX.push(randomX)
-      arrY.push(randomY)
-      if(k >  n*n / 4)
-      {
-        break;
-      }
-    }
-  } 
-}
-for (let i = 0; i < n*n / 80; i++) {
-  let randomX = Math.floor(Math.random() * n)
-  let randomY = Math.floor(Math.random() * n)
-  for(let k = 0; k < arrX.length; k++){
-    if(arrX[k] != randomX && arrY[k] != randomY){
-      matrix[randomX][randomY] = 3
-      if(k >  n*n / 15)
-      {
-        break;
-      }
-    }
-  }
-}
-for (let i = 0; i < n*n / 100; i++) {
-  let randomX = Math.floor(Math.random() * n)
-  let randomY = Math.floor(Math.random() * n)
-  for(let k = 0; k < arrX.length; k++){
-    if(arrX[k] != randomX && arrY[k] != randomY){
-      matrix[randomX][randomY] = 5
-      if(k >  n*n / 15)
-      {
-        break;
-      }
-    }
-  }
-}
-
-
-
 function setup() {
-  createCanvas(matrix[0].length * side, matrix.length * side);
-  background("grey");
-  frameRate(1);
-  for (var x = 0; x < matrix.length; x++) {
-    for (var y = 0; y < matrix[x].length; y++) {
-      if (matrix[x][y] == 1) {
-        var gr = new Grass(x, y); // {this.x: 0, this.y: 2, }
-        grassArr.push(gr);
-      } else if (matrix[x][y] == 2) {
-        var grEat = new GrassEater(x, y); // {this.x: 0, this.y: 2, }
-        grassEaterArr.push(grEat);
-      }
-      else if (matrix[x][y] == 3) {
-        var grEat = new Gazanik(x, y); // {this.x: 0, this.y: 2, }
-        gazanikArr.push(grEat);
-      }
-      
-      else if (matrix[x][y] == 5) {
-        var finder = new bombFinder(x, y); 
-        findArr.push(finder);
-      }
+
+    var socket = io();
+
+    var side = 30;
+
+    var matrix = [];
+
+    //! Getting DOM objects (HTML elements)
+    let grassCountElement = document.getElementById('grassCount');
+    let grassEaterCountElement = document.getElementById('grassEaterCount');
+
+    //! adding socket listener on "data" <-- name, after that fire 'drawCreatures' function 
+
+    socket.on("data", drawCreatures);
+
+    function drawCreatures(data) {
+        //! after getting data pass it to matrix variable
+        matrix = data.matrix;
+        grassCountElement.innerText = data.grassCounter;
+        grassEaterCountElement.innerText = data.grassEaterCounter;
+        //! Every time it creates new Canvas woth new matrix size
+        createCanvas(matrix[0].length * side, matrix.length * side)
+        //! clearing background by setting it to new grey color
+        background('#acacac');
+        //! Draw grassCount and grassEaterCount to HTML (use DOM objects to update information, yes, and use .innerText <- function)
+
+        //! Drawing and coloring RECTs
+        for (var i = 0; i < matrix.length; i++) {
+            for (var j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 1) {
+                    fill("green");
+                    rect(j * side, i * side, side, side);
+                } else if (matrix[i][j] == 2) {
+                    fill("orange");
+                    rect(j * side, i * side, side, side);
+                } else if (matrix[i][j] == 0) {
+                    fill('#acacac');
+                    rect(j * side, i * side, side, side);
+                } else if (matrix[i][j] == 3) {
+                    fill('red');
+                    rect(j * side, i * side, side, side);
+                } else if (matrix[i][j] == 4) {
+                    fill('blue');
+                    rect(j * side, i * side, side, side);
+                } else if (matrix[i][j] == 5) {
+                    fill('yellow');
+                    rect(j * side, i * side, side, side);
+                }
+            }
+        }
     }
-  }
-}
-
-
-function draw() {
-  for (let x = 0; x < matrix.length; x++) {
-    for (let y = 0; y < matrix[x].length; y++) {
-      if (matrix[x][y] == 0) {
-        fill("grey");
-      } else if (matrix[x][y] == 1) {
-        fill("green");
-      } else if (matrix[x][y] == 2) {
-        fill("yellow");
-      } else if (matrix[x][y] == 3) {
-        fill("red");
-      } else if (matrix[x][y] == 4) {
-        fill("orange");
-      } else if (matrix[x][y] == 5) {
-        fill('black')
-      }
-      rect(y * side, x * side, side, side);
-    }
-  }
-
-  for (const i in grassArr) {
-    grassArr[i].mul();
-  }
-  for (const i in grassEaterArr) {
-    grassEaterArr[i].mul()
-    grassEaterArr[i].eat();
-  }
-  for (const i in gazanikArr) {
-    gazanikArr[i].eat()
-  }
-
-  if (gazanikArr.length >= 14 ) {
-    var bomb = new atomicBomb()
-    bomb.boom()
-    for(let i in  atomikBomb){
-      atomikBomb[i].gmp()
-    }
-  }
-  for(const i in findArr){
-    findArr[i].move()
-    findArr[i].eat()
-  }  
 }
--- server.js
+++ server.js
@@ -1,4 +1,71 @@
+var Grass = require("./modules/Grass.js");
+var GrassEater = require("./modules/GrassEater.js");
+var AtomicBomb = require("./modules/AtomicBomb.js");
+var Gazanik = require("./modules/Gazanik.js");
+var BombFinder = require("./modules/bombFinder.js");
+let random = require('./modules/random');
+//! Requiring modules  --  END
+
+
+//! Setting global arrays  --  START
+grassArr = [];
+grassEaterArr = [];
+atomicBombArr = [];
+atArr = [];
+atomicArr = [];
+gazanikArr = [];
+bombFinderArr = [];
+matrix = [];
+grassHashiv = 0;
+var grassEaterHashiv;
+//! Setting global arrays  -- END
+
+
+
+
+//! Creating MATRIX -- START
+function matrixGenerator(matrixSize, grass, grassEater, atomicBomb, gazanik, bombFinder) {
+    for (let i = 0; i < matrixSize; i++) {
+        matrix[i] = [];
+        for (let o = 0; o < matrixSize; o++) {
+            matrix[i][o] = 0;
+        }
+    }
+    for (let i = 0; i < grass; i++) {
+        let customX = Math.floor(random(matrixSize)); // 0-9
+        let customY = Math.floor(random(matrixSize)); // 4
+        matrix[customY][customX] = 1;
+    }
+    for (let i = 0; i < grassEater; i++) {
+        let customX = Math.floor(random(matrixSize));
+        let customY = Math.floor(random(matrixSize));
+        matrix[customY][customX] = 2;
+    }
+    for (let i = 0; i < atomicBomb; i++) {
+        let customX = Math.floor(random(matrixSize));
+        let customY = Math.floor(random(matrixSize));
+        matrix[customY][customX] = 4;
+    }
+    for (let i = 0; i < gazanik; i++) {
+        let customX = Math.floor(random(matrixSize));
+        let customY = Math.floor(random(matrixSize));
+        matrix[customY][customX] = 3;
+    }
+    for (let i = 0; i < bombFinder; i++) {
+        let customX = Math.floor(random(matrixSize));
+        let customY = Math.floor(random(matrixSize));
+        matrix[customY][customX] = 5;
+    }
+}
+matrixGenerator(20, 20, 10, 5 , 15, 3);
+//console.log(matrix)
+//! Creating MATRIX -- END
+
+
+
+//! SERVER STUFF  --  START
 var express = require('express');
+const bombFinder = require("./modules/bombFinder.js");
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
@@ -7,3 +74,116 @@ app.get('/', function (req, res) {
     res.redirect('index.html');
 });
 server.listen(3000);
+//! SERVER STUFF END  --  END
+
+
+
+function creatingObjects() {
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] == 1) {
+                var grass = new Grass(x, y);
+                grassArr.push(grass);
+                grassHashiv++;
+                console.log(grassHashiv);
+                //grassHashiv++;
+            }
+            else if (matrix[y][x] == 2) {
+                var grassEater = new GrassEater(x, y);
+                grassEaterArr.push(grassEater);
+                
+            }
+            else if (matrix[y][x] == 4) {
+                var atomicBomb = new AtomicBomb(x, y);
+                atomicArr.push(atomicBomb);
+            }
+            else if (matrix[y][x] == 3) {
+                var gazanik = new Gazanik(x, y);
+                gazanikArr.push(gazanik);
+                //console.log(gazanikArr);
+            }
+            else if (matrix[y][x] == 5) {
+                var bombFinder = new BombFinder(x, y);
+                bombFinderArr.push(bombFinder);
+            }
+        }
+    }
+}
+console.log(gazanikArr)
+creatingObjects();
+
+function game() {
+    if (grassArr[0] !== undefined) {
+        for (var i in grassArr) {
+            grassArr[i].mul();
+            //console.log(grassArr.length);
+            //console.log(i);
+            //grassHashiv = grassArr.length;
+        }
+    }
+    if (grassEaterArr[0] !== undefined) {
+        for (var i in grassEaterArr) {
+            grassEaterArr[i].eat();
+            grassEaterHashiv = i;
+        }
+    }
+    if (gazanikArr[0] !== undefined) {
+        for (var i in gazanikArr) {
+            gazanikArr[i].eat();
+            //gazanikArr[i].move();
+            //gazanikArr[i].die();
+            //console.log(gazanikArr)
+        }
+    }/*
+    if (atomicArr[0] !== undefined) {
+        for (var i in atomicArr) {
+            //atomicArr[i].boom();
+            atomicBombArr[i].gmp();
+            //gazanikArr[i].move();
+            //gazanikArr[i].die();
+            //console.log(gazanikArr)
+        }
+    }*/
+    if (bombFinderArr[0] !== undefined) {
+        for (var i in bombFinderArr) {
+            bombFinderArr[i].eat();
+            bombFinderArr[i].move();
+            //gazanikArr[i].move();
+            //gazanikArr[i].die();
+            //console.log(gazanikArr)
+        }
+    }
+    if (gazanikArr.length >= 14 ) {
+        var bomb = new AtomicBomb()
+        bomb.boom()
+        for(let i in  atomicBombArr){
+            atomicBombArr[i].gmp()
+        }
+    }
+      
+    /*
+    if (grassEaterArr[0] !== undefined) {
+        for (var i in grassEaterArr) {
+            grassEaterArr[i].eat();
+        }
+    }
+    if (grassEaterArr[0] !== undefined) {
+        for (var i in grassEaterArr) {
+            grassEaterArr[i].eat();
+        }
+    }*/
+
+    //! Object to send
+    let sendData = {
+        matrix: matrix,
+        grassCounter: grassHashiv,
+        grassEaterCounter:grassEaterHashiv
+    }
+
+    //! Send data over the socket to clients who listens "data"
+    io.sockets.emit("data", sendData);
+}
+
+
+
+setInterval(game, 1000)
